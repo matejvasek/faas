0934ddda7d0adaaaf008a1e429eb6bcbd27aa095
Matej Vasek <mvasek@redhat.com>
2022-07-29 16:52:34 +0200

    Debug Quarkus Platform auto-update
    Signed-off-by: Matej Vasek <mvasek@redhat.com>

---
+++ hack/update-quarkus-platform.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+LATEST_PLATFORM="$(curl -s "https://code.quarkus.io/api/platforms" | \
+  jq -r '.platforms[0].streams[0].releases[0].quarkusCoreVersion')"
+
+echo "$LATEST_PLATFORM"
+
+XPATH='//*[local-name()="quarkus.platform.version"]/text()'
+
+CE_TEMPLATE_PLATFORM="$(xmllint --xpath "$XPATH" templates/quarkus/cloudevents/pom.xml)"
+HTTP_TEMPLATE_PLATFORM="$(xmllint --xpath "$XPATH" templates/quarkus/cloudevents/pom.xml)"
+
+echo "$CE_TEMPLATE_PLATFORM"
+echo "$HTTP_TEMPLATE_PLATFORM"
+
+if [ "$CE_TEMPLATE_PLATFORM" == "$LATEST_PLATFORM" ] && \
+  [ "$HTTP_TEMPLATE_PLATFORM" == "$LATEST_PLATFORM" ]; then
+  echo "Everything is up-to-date!"
+  exit 0
+fi
+
+sed -i -E "s#<quarkus.platform.version>.+</quarkus.platform.version>#<quarkus.platform.version>${LATEST_PLATFORM}</quarkus.platform.version>#g" \
+  ./templates/quarkus/cloudevents/pom.xml
+sed -i -E "s#<quarkus.platform.version>.+</quarkus.platform.version>#<quarkus.platform.version>${LATEST_PLATFORM}</quarkus.platform.version>#g" \
+  ./templates/quarkus/http/pom.xml
+make zz_filesystem_generated.go
+
+BRANCH="update-quarkus-platform-${LATEST_PLATFORM}"
+
+git config --global user.email "quarkus-bot@example.com"
+git config --global user.name "quarkus-platform-update-bot"
+git checkout -b "$BRANCH"
+git add ./templates/quarkus/cloudevents/pom.xml ./templates/quarkus/http/pom.xml zz_filesystem_generated.go
+git commit -m "chore: update Quarkus platform to $LATEST_PLATFORM"
+git push --set-upstream origin "$BRANCH"
+
+curl -v \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  https://api.github.com/repos/matejvasek/faas/pulls \
+  -d "{\"title\":\"chore: update Quarkus Platform to $LATEST_PLATFORM\",\"body\":\"chore: update Quarkus Platform to $LATEST_PLATFORM\",\"head\":\"matejvasek:$BRANCH\",\"base\":\"master\"}"
