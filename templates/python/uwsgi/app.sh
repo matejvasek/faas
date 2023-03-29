a69ffc4a8978bde37e50bd6ec0a82c3dd1aa6ab3
Lumir Balhar <lbalhar@redhat.com>
2023-03-29 11:59:30 +0200

    Add Python templates for gunicorn and uwsgi + Flask
    These templates represents the most common and simple ways
    how to implement FAAS in Python either directly via gunicorn
    or using Flask and uwsgi.

---
+++ templates/python/uwsgi/app.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+exec uwsgi \
+    --http-socket :8080 \
+    --die-on-term \
+    --master \
+    --single-interpreter \
+    --enable-threads \
+    --threads=5 \
+    --thunder-lock \
+    --module func
