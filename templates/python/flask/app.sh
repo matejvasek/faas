1ed4d03a2835209842be99b3ad327877e84bebe0
Matej Vasek <mvasek@redhat.com>
2023-04-03 22:23:28 +0200

    fixup: use gunicorn
    Signed-off-by: Matej Vasek <mvasek@redhat.com>

--- templates/python/flask/app.sh
+++ templates/python/flask/app.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-exec uwsgi \
-    --http-socket :8080 \
-    --die-on-term \
-    --master \
-    --single-interpreter \
-    --enable-threads \
-    --threads=5 \
-    --thunder-lock \
-    --module func
+exec gunicorn func:application --bind=0.0.0.0:8080 --access-logfile=-
