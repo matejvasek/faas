cf35c2a99adf892b0e6e4ed553f214d8c138ea5d
Matej Vasek <mvasek@redhat.com>
2023-04-04 04:10:17 +0200

    fixup: updated messages
    Signed-off-by: Matej Vasek <mvasek@redhat.com>

--- templates/python/flask/func.py
+++ templates/python/flask/func.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello():
-    return b'Hello World from uWSGI hosted WSGI application!'
+    return b'Hello World from Flask application!'
 
 
 if __name__ == '__main__':
